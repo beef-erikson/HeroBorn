9f9dcbc31873de778d0202268670d0cb87b0c448
beef-erikson <beef.erikson.studios@gmail.com>
2023-03-02 23:45:31 -0500

    Changed access of _inventory in Shop.cs.

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -3,15 +3,15 @@ using System.Linq;
 
 public class Shop<T> where T : Collectable
 {
-    public readonly List<T> inventory = new List<T>();
+    private readonly List<T> _inventory = new List<T>();
 
     public void AddItem(T newItem)
     {
-        inventory.Add(newItem);
+        _inventory.Add(newItem);
     }
 
     public int GetStackCount<TU>() where TU : T
     {
-        return inventory.OfType<TU>().Count();
+        return _inventory.OfType<TU>().Count();
     }
 }
